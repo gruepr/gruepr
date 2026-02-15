686e0e4062e27a38ce088c30ebc2609156a7aa67
Joshua Hertz <44120268+joshuahertz@users.noreply.github.com>
2026-02-14 20:33:16 -0500

    Update googlesecrets.h

--- LMS/googlesecrets.h
+++ LMS/googlesecrets.h
@@ -1,4 +1,2 @@
-#ifdef GITHUB_SECRETS
-    inline static const char CLIENT_ID[]{"SECRET_GOOGLE_CLIENT_ID"};
-    inline static const char CLIENT_SECRET[]{"SECRET_GOOGLE_CLIENT_SECRET"};
-#endif
+inline static const char CLIENT_ID[]{"SECRET_GOOGLE_CLIENT_ID"};
+inline static const char CLIENT_SECRET[]{"SECRET_GOOGLE_CLIENT_SECRET"};
